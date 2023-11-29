c89ee921ba741f92808e3e057f028b0a4add9cca
Ubaldo Enriquez <uvam@hotmail.es>
2023-11-29 16:37:25 +0000

    Commit made from VS Code Azure Static Web Apps

--- src/App.jsx
+++ src/App.jsx
@@ -24,12 +24,12 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
+  name: "Ubaldo Enriquez",
+  title: "Full stack developer",
+  email: "uva@",
   gitHub: "microsoft",
   instagram: "microsoft",
-  linkedIn: "satyanadella",
+  linkedIn: "ubaldo E",
   medium: "",
   twitter: "microsoft",
   youTube: "Code",
