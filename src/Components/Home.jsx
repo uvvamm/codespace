be12b2d780e9790910d4760549f11cda2f4fff3b
Ubaldo Enriquez <uvam@hotmail.es>
2023-11-29 16:51:51 +0000

    dsf

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -29,7 +29,7 @@ const Home = ({ name, title }) => {
     <section id="home" className="min-height">
       <img className="background" src={image} alt="" />
       <div
-        style={{ position: "absolute", top: "5rem", left: "2rem", width: "17rem", color: "white" }}
+        style={{ position: "absolute", top: "5rem", left: "2rem", width: "17rem", color: "white"  }}
       >
         <h1>{name}</h1>
         <h2>{title}</h2>
